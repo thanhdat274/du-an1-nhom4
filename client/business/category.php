498c8e4f35232aad32de4a4b96d18efb41e29402
datntph13533 <77159701+thanhdat274@users.noreply.github.com>
2022-01-03 11:16:44 +0700

    Revert "update code"
    This reverts commit d566bd752cff96bcb3e19f26948d514a665c8013.

--- client/business/category.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    function list_product(){
-        $contents =  "Danh sach san pham theo danh muc";
-        $mainView = "./client/views/category/index.php";
-        include_once "./client/views/layouts/main.php";
-    }
-
-?>
